36e2dd711ac8fc3b8dcb4addf17f6f0bc18fc0ef
Gagan Khandate <gagankhandate@gmail.com>
2018-12-08 14:11:33 -0500

    partially implemented get_series() in kitti_utils.py

--- deep_visual_odometry/kitti_utils.py
+++ deep_visual_odometry/kitti_utils.py
@@ -1,5 +1,5 @@
 import pykitti
-
+import numpy as np
 # load data set using pykitti
 
 class KITTIdata():
@@ -7,11 +7,17 @@ class KITTIdata():
     ignore the first image for differing
 
     """
-    def __init__(self, basedir, sequences):
-        self.sequence_nos = sequences
+    def __init__(self, basedir, sequences, img_size = None):
+        self.sequences = sequences
+        self.img_size = img_size
         self.dataset = {}
+        self.dataset_len = {}
+        self.img_idx = {}
+        self.seq_idx = 0
         for sequence in sequences:
             self.dataset[sequence] =  pykitti.odometry(basedir, sequence)
+            self.dataset_len[sequence] = len(self.dataset[sequence].cam2_files)
+            self.img_idx[sequence] = 0
 
     def _process(self, images):
         # images is a tuple = (prev_image, curr__image)
@@ -28,7 +34,45 @@ class KITTIdata():
             pass
 
     def get_series(self, sequence_len = 100, sequences = None):
-        pass
+        if sequences is None:
+           sequences = self.sequences
+        while self.sequences[self.seq_idx] not in sequences:
+            self.seq_idx += 1
+        sequence = self.sequences[self.seq_idx]
+
+        # input
+        images = []
+        for i in range(sequence_len + 1):
+            image = self.dataset[sequence].get_cam2(i + self.img_idx[sequence])
+            if self.img_size is not None:
+                image = image.resize(size=self.img_size)
+            image = np.array(image, dtype=np.uint8)
+            images.append(image)
+
+        image_plus_diff_images = []
+        for i in range(sequence_len):
+            diff_image = images[i+1] - images[i]
+            image_plus_diff_images.append(np.concatenate((images[i], diff_image), axis = 2))
+        series_input = np.stack(image_plus_diff_images)
+
+        # TODO: ground truth poses
+        self.img_idx[sequence] += 1
+        if self.img_idx[sequence] + sequence_len >= self.dataset_len[sequence] - 1:
+            self.img_idx[sequence] = 0
+        while self.sequences[self.seq_idx] not in sequences:
+            self.seq_idx+= 1
+
+        #TODO: return ground truth poses
+        return series_input
+
+
+
+
+
+
+
+
+
 
 
 
