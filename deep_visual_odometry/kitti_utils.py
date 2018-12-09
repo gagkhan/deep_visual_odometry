b827f449f5bf948b61c3fb0600e30792375fd571
Gagan Khandate <gagankhandate@gmail.com>
2018-12-09 14:55:33 -0500

    optimized KITTIData get_series() and get_series_batch()

--- deep_visual_odometry/kitti_utils.py
+++ deep_visual_odometry/kitti_utils.py
@@ -1,22 +1,73 @@
 import pykitti
 import numpy as np
 # load data set using pykitti
+
+def angle(T):
+    R = T[:3, :3]
+    return np.arctan2(R[1, 0], R[0, 0])
+
+def get_vel(T_prev, T_curr):
+    delta_T = np.dot(np.linalg.inv(T_prev), T_curr)
+    delta_rot = angle(delta_T)
+    delta_pos = np.linalg.norm(delta_T[0:3, 3])
+    return np.array([delta_pos, delta_rot])
+
+def get_pose(T):
+    x = T[0, 3]
+    y = T[1, 3]
+    theta = angle(T)
+    return np.array([x, y, theta])
+
 class KITTIdata():
     """
-    ignore the first image for differing
+
 
     """
     def __init__(self, basedir, sequences, img_size = None):
         self.sequences = sequences
         self.img_size = img_size
+
         self.dataset = {}
         self.dataset_len = {}
+
         self.img_idx = {}
         self.seq_idx = 0
+
+        self.input = {}
+        self.velocities = {}
+        self.poses = {}
+
         for sequence in sequences:
-            self.dataset[sequence] =  pykitti.odometry(basedir, sequence)
+
+            dataset = pykitti.odometry(basedir, sequence)
+            self.dataset[sequence] =  dataset
             self.dataset_len[sequence] = len(self.dataset[sequence].cam2_files)
             self.img_idx[sequence] = 0
+
+            input = []
+            velocities = []
+            poses = []
+
+            image_prev = dataset.get_cam2(0)
+            if self.img_size is not None:
+                image_prev = image_prev.resize(size = self.img_size)
+            image_prev = np.array(image_prev, dtype=np.uint8)
+
+            for i in range(1, self.dataset_len[sequence]):
+                image = dataset.get_cam2(i)
+                if self.img_size is not None:
+                    image = image.resize(size=self.img_size)
+                image = np.array(image, dtype=np.uint8)
+                diff_image = image - image_prev
+                input.append(np.concatenate((image, diff_image), axis = 2))
+                image_prev = image
+                velocities.append(get_vel(dataset.poses[i-1], dataset.poses[i]))
+                poses.append(get_pose(dataset.poses[i]))
+
+            self.input[sequence] = np.stack(input)
+            self.velocities[sequence] = np.stack(velocities)
+            self.poses[sequence] = np.stack(poses)
+            print('completed load sequence {} data'.format(sequence))
 
     def get_series_batch(self, sequence_len = 100,  batch_size = 10, sequences = None):
         batch_input = []
@@ -36,53 +87,12 @@ class KITTIdata():
         self.seq_idx %= len(self.sequences)
         while self.sequences[self.seq_idx] not in sequences:
             self.seq_idx += 1
+
         sequence = self.sequences[self.seq_idx]
-
-        # input
-        images = []
-        for i in range(sequence_len + 1):
-            image = self.dataset[sequence].get_cam2(i + self.img_idx[sequence])
-            if self.img_size is not None:
-                image = image.resize(size=self.img_size)
-            image = np.array(image, dtype=np.uint8)
-            images.append(image)
-
-        image_plus_diff_images = []
-        for i in range(sequence_len):
-            diff_image = images[i+1] - images[i]
-            image_plus_diff_images.append(np.concatenate((images[i+1], diff_image), axis = 2))
-        series_input = np.stack(image_plus_diff_images)
-
-        # def normalize_cols(M):
-        #     for i in range(M.shape[1]):
-        #         M[:, i] /= np.linalg.norm(M[:, i])
-        #     return M
-
-        def angle(T):
-            R = T[:3, :3]
-            return np.arctan2(R[1, 0], R[0, 0])
-
-        def get_vel(T_prev, T_curr):
-            delta_T = np.dot(np.linalg.inv(T_prev), T_curr)
-            delta_rot = angle(delta_T)
-            delta_pos = np.linalg.norm(delta_T[0:3, 3])
-            return np.array([delta_pos, delta_rot])
-
-        def get_pose(T):
-            x = T[0,3]
-            y = T[1,3]
-            theta = angle(T)
-            return np.array([x, y, theta])
-
-        # poses and estimated velocity
-        poses = []
-        velocities = []
-        dataset_poses = self.dataset[sequence].poses
-        for i in range(sequence_len):
-            velocities.append(get_vel(dataset_poses[i],dataset_poses[i+1]))
-            poses.append(get_pose(dataset_poses[i+1]))
-        poses = np.stack(poses)
-        velocities = np.stack(velocities)
+        idx = self.img_idx[sequence]
+        series_input = self.input[sequence][idx: idx + sequence_len]
+        velocities = self.velocities[sequence][idx: idx + sequence_len]
+        poses = self.poses[sequence][idx: idx + sequence_len]
 
         self.img_idx[sequence] += 1
         if self.img_idx[sequence] + sequence_len >= self.dataset_len[sequence] - 1:
@@ -90,7 +100,6 @@ class KITTIdata():
         self.seq_idx+= 1
 
         return series_input, velocities, poses
-
 
 
 
